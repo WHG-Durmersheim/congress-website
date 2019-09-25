ce5d7521181b5d01071ce4a6bc7c77082ff2bb8f
Domi04151309 <domi04151309business@gmail.com>
2019-09-25 10:42:38 +0200

    Update recordings.php

--- pages/recordings.php
+++ pages/recordings.php
@@ -2,13 +2,26 @@
 require_once(($_SERVER['DOCUMENT_ROOT']).'/layout/PageLayout.php');
 if (!isset($TPL)) {
   $TPL = new PageLayout();
-  $TPL->PageTitle = 'Recordings';
+  $TPL->PageTitle = 'Aufnahmen';
   $TPL->PageContent = __FILE__;
   include ($_SERVER['DOCUMENT_ROOT']).'/layout/layout.php';
   exit;
 }
 ?>
-<h2>Recordings</h2>
+<style>
+.video {
+  width: calc(100vw - 32px);
+  height: calc(56vw - 32px);
+  background: #000;
+}
+@media only screen and (min-width: 720px) {
+  .video {
+    width: 688px;
+    height: 387px;
+  }
+}
+</style>
+<h2>Aufnahmen</h2>
 <p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/5_CpeZEiIFQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe title="Livestream" class="video" src="https://www.youtube.com/embed/5_CpeZEiIFQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </p>
