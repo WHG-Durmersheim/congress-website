a8e86194b1b6e73d8558b89354dac0e8f8a558d9
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-09-26 20:10:13 +0200

    Create Livestreaming Page and add Super Secret Streaming URL

--- pages/livestreaming.php
+++ pages/livestreaming.php
@@ -11,6 +11,7 @@ if (!isset($TPL)) {
 <link href="//vjs.zencdn.net/5.8/video-js.min.css" rel="stylesheet">
     <script src="//vjs.zencdn.net/5.8/video.min.js"></script>
 <style>
+
 .video {
   width: calc(100vw - 32px);
   height: calc(56vw - 32px);
@@ -23,10 +24,9 @@ if (!isset($TPL)) {
   }
 }
 </style>
-<h2>Aufnahmen</h2>
+<h2>Livestream</h2>
 <p>
-  <video id="player" class="video-js vjs-default-skin" height="360" width="640" controls preload="none">
-    <source src="http://localhost:8080/hls/stream.m3u8" type="application/x-mpegURL" />
-</video>
-  <iframe title="Livestream" class="video" src="https://www.youtube.com/embed/5_CpeZEiIFQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <video title="Livestream" id="player" class="video video-js vjs-default-skin" controls preload="none">
+    <source src="http://116.203.221.72:8080/hls/stream.m3u8" type="application/x-mpegURL" />
+  </video>
 </p>
