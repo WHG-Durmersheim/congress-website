5fb5bbd865f190b039c37007a4c685b231baa6ea
Domi04151309 <domi04151309business@gmail.com>
2019-09-28 18:56:07 +0200

    Add map dummy content

--- pages/map.php
+++ pages/map.php
@@ -9,6 +9,40 @@ if (!isset($TPL)) {
 }
 ?>
 <h2>Karte</h2>
+<img alt="Karte" src="/images/map.jpg">
+<h2>Aussteller</h2>
+<h2>Cornelsen</h2>
 <p>
-  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>Klett</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>Mediengestützte Didaktik</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>RBI Rittershofer GmbH (Medieneinheiten)</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>Sch&uuml;lermedienmentoren</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>St&uuml;ber Systems GmbH (EZCast)</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
+</p>
+<h2>Viscom (Medieneinheiten)</h2>
+<p>
+  Raum: 000<br>
+  Eine kleine Beschreibung des Ausstellers.
 </p>
