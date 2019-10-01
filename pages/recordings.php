cc1143bfa0c59b14aa3c8bb23b061e1f89a267bd
Dominik <36272047+Domi04151309@users.noreply.github.com>
2019-10-01 09:15:40 +0200

    Fix styles loading in recordings.php

--- pages/recordings.php
+++ pages/recordings.php
@@ -10,21 +10,8 @@ if (!isset($TPL)) {
 ?>
 
 <link href="//vjs.zencdn.net/5.8/video-js.min.css" rel="stylesheet">
-    <script src="//vjs.zencdn.net/5.8/video.min.js"></script>
-<style>
-
-.video {
-  width: calc(100vw - 32px);
-  height: calc(56vw - 32px);
-  background: #000;
-}
-@media only screen and (min-width: 720px) {
-  .video {
-    width: 688px;
-    height: 387px;
-  }
-}
-</style>
+<script src="//vjs.zencdn.net/5.8/video.min.js"></script>
+<style><?php include($_SERVER['DOCUMENT_ROOT']).'/css/video.min.css'; ?></style>
 <h2>Aufnahmen</h2>
 <p>
   <video title="Livestream" id="player" class="video video-js vjs-default-skin" controls preload="none">
