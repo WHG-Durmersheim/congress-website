8e8a972d831f9424209f8f8129e9dee9b7283a20
Dominik <36272047+Domi04151309@users.noreply.github.com>
2019-10-07 15:30:19 +0200

    Add text from flyer to index.php

--- index.php
+++ index.php
@@ -15,18 +15,18 @@ if (!isset($TPL)) {
 </p>
 <h2>Home</h2>
 <p>
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-</p>
-<h2>Lorem ipsum</h2>
-<p>
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-</p>
-<h2>Duis autem vel</h2>
-<p>
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-</p>
-<h2>Ut wisi enim ad</h2>
-<p>
-Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+  Menschen denken nicht algorithmisch und Bildung ist nicht digital.
+  Und dennoch können digitale Medien beim schulischen Bildungsprozess von jungen Menschen eine besondere Rolle spielen.
+  <br><br>
+  Hierbei scheint es wesentlich, dass digitale Medien nicht im Zentrum, sondern im Dienst dieses Bildungsprozesses stehen.<br>
+  Damit Schulen der Herausforderung der unterstützenden Einbindung digitaler Prozesse in die traditionelle Unterrichtsdidaktik und deren Erweiterung durch neue technische Möglichkeiten gewachsen sein können,
+  spielen zahlreiche Gelingensfaktoren eine wesentliche Rolle.
+  Diese reichen von der technischen Infrastruktur einer Lernlandschaft mit einer sinnvollen Verzahnung von eingesetzten digitalen Lern-/Lehrlösungen über das Changemanagement im Rahmen der Schulentwicklung bis zur Unterstützung der Lehrer und Schüler beim Einsatz dieser Methoden im Bildungsprozess durch spezifisch abgestimmte Qualifizierungsmaßnahmen.<br>
+  Am Wilhelm-Hausenstein-Gymnasium in Durmersheim wurde vor mehreren Jahren eine Arbeitsgruppe mit dem Titel „Bildung im digitalen Zeitalter“ ins Leben gerufen.
+  Begleitet von Kooperationen aus dem Hochschulbereich, der Politik und der Wirtschaft versucht diese gemeinsam mit der Schulleitung den zahlreichen Dimensionen einer gelingenden Implementierung von digitalen Lern-/Lehrformaten gerecht zu werden.
+  <br><br>
+  Die Veranstaltung „Schule in der digitalen Dimension“ soll ein Versuch sein, alle Facetten der Digitalisierung an der Schule an einem Tag für Besucher sicht- und nachvollziehbar zu machen sowie Möglichkeiten zum vertieften Einblick in konkrete an der Schule eingesetzte Infrastruktur, Technik, Methodik und Begleitungsformate zu bieten.
+  Auf dieser Grundlage soll ein Forum des Austauschs entstehen, das Chancen, Grenzen und Umsetzungsmöglichkeiten von digitalen Elementen im Bildungsprozess zum Thema hat.
+  <br><br>
+  Angesprochen werden sollen neben Kollegen und Schulleitungen vor allem auch politische Entscheidungsträger und Schulverwaltungsmitglieder, die durch ihr Wirken wesentliche Weichenstellungen in der Bildungslandschaft vornehmen und Entwicklungsprozesse nachhaltig beeinflussen.
 </p>
