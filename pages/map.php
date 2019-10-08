81ee26797427d4e61a92e82c186530937131d7bd
Dominik <36272047+Domi04151309@users.noreply.github.com>
2019-10-08 08:24:30 +0200

    Add descriptions to map.php

--- pages/map.php
+++ pages/map.php
@@ -20,93 +20,96 @@ if (!isset($TPL)) {
 <h2>co.Tec</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Office 365 für Schule/Lehrer/Schüler – Schullizenz
 </p>
 <h2>heinekingmedia</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Das Digitale Schwarze Brett (DSB) als Informationsplattform mit App
 </p>
 <h2>OctoGate</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  WLAN-Lösung für Schulen
 </p>
 <h2>RBI Rittershofer</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Medienwägen für die Klassenzimmer und Konferenzen
 </p>
 <h2>St&uuml;ber Systems Berlin</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Kabellose Bildschirmpräsentation mit EZ-Cast Pro2
 </p>
 <h2>Untis BW</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Stunden- und Vertretungsplanung.
+  Apps und Software für Vertretungsplan, Kommunikation und Dateientausch in Lerngruppen
 </p>
 <h2>Visucom</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Klassenzimmertechnik
 </p>
 <h2><br>Didaktik und Unterricht</h2>
 <h2>Cornelsen</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Schulbücher und ihre digitalen Hilfsmittel für Unterrichtsvorbereitung und -durchführung
 </p>
 <h2>Flip the Classroom</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Einsatz und Erstellung von Erklärvideos im Unterricht
 </p>
 <h2>Hochschule der Medien Stuttgart</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Digitale Lehr- und Lernformen
 </p>
 <h2>Klett</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Schulbücher und ihre digitalen Hilfsmittel für Unterrichtsvorbereitung und -durchführung
 </p>
 <h2>Lehrer berichten</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Best Practice aus dem Unterrichtsalltag
 </p>
 <h2>Medienzentrum Mittelbaden SESAM</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Lehrfilme für den Unterricht zum Streamen, Downloaden und Teilen mit den Schülern
 </p>
 <h2><br>Konzeption und Umsetzung</h2>
 <h2>Arbeitsgruppe &bdquo;Bildung im digitalen Zeitalter&rdquo;</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Fortbildungskonzept, Implementierung digitaler Arbeitsfomen, Wartung, Fortbildung, Gestaltung
 </p>
 <h2>LRA Rastatt</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  IT-Entwicklungsplan und Begleitung der Kreisschulen
 </p>
 <h2>Medienzentrum Mittelbaden MEP</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Inhalte und Erstellung eines Medienentwicklungsplans am Beispiel des WHGs.
+  3D-Druck und VR.
+  Möglichkeiten im Unterricht und Expertise
 </p>
 <h2>Schulleitung</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Digitalisierung im Kontext des allgemeinen Bildungsauftrags am Gymnasium
 </p>
 <h2>Studienseminar Karlsruhe</h2>
 <p>
   Raum: 000<br>
-  Eine kleine Beschreibung des Ausstellers.
+  Seminar für Ausbildung und Fortbildung der Lehrkräfte (Gymnasium)
 </p>
 <script src="/js/map.min.js" async></script>
