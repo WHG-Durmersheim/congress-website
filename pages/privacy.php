7fd7b10e1f08f2a4266cbb9465bcce3fc2a18ebc
Domi04151309 <domi04151309business@gmail.com>
2019-10-12 13:02:09 +0200

    Improve privacy policy look

--- pages/privacy.php
+++ pages/privacy.php
@@ -9,39 +9,22 @@ if (!isset($TPL)) {
 }
 ?>
 <h2>Datenschutzerkl&auml;rung</h2>
-<div>
-<h4>1. Verantwortlicher und Datenschutzbeauftragter</h4>
-
-<div>1.1 Verantwortlicher gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (EU-DSGVO) ist</div>
-
-<p>T. Dornblüth (Schulleiter)</p>
-
-<p>Wilhelm-Hausenstein-Gymnasium Durmersheim</p>
-
-<p>Weißenburger Straße 42</p>
-
-<p>76448 Durmersheim</p>
-
-<p>Tel.: 07245 / 919 66-0</p>
-
-<p>Fax : 07245 / 919 66-11</p>
-
-<p>Email: direktion@whg-durmersheim.de</p>
-
+<h2>1. Verantwortlicher und Datenschutzbeauftragter</h2>
+<p>1.1 Verantwortlicher gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (EU-DSGVO) ist</p>
+<p>T. Dornblüth (Schulleiter)<br>
+Wilhelm-Hausenstein-Gymnasium Durmersheim<br>
+Weißenburger Straße 42<br>
+76448 Durmersheim<br>
+Tel.: 07245 / 919 66-0<br>
+Fax : 07245 / 919 66-11<br>
+Email: direktion@whg-durmersheim.de</p>
 <p>1.2 Unseren Datenschutzbeauftragten erreichen Sie unter datenschutzbeauftragter@whg-durmersheim.de oder unserer Postadresse mit dem Zusatz „der Datenschutzbeauftragte“.</p>
-
-<h4>2. Information über die Erhebung personenbezogener Daten, Speicherdauer</h4>
-
+<h2>2. Information über die Erhebung personenbezogener Daten, Speicherdauer</h2>
 <p>2.1 Im Folgenden informieren wir über die Erhebung personenbezogener Daten bei Nutzung unserer Website. Personenbezogene Daten sind alle Daten, die auf Sie persönlich beziehbar sind, z. B. Name, Adresse, E-Mail-Adressen, Nutzerverhalten.</p>
-
 <p>2.2 Bei Ihrer Kontaktaufnahme mit uns per E-Mail oder über ein Kontaktformular werden die von Ihnen mitgeteilten Daten (Ihre E-Mail-Adresse, ggf. Ihr Name und Ihre Telefonnummer) zum einen kurzzeitig auf dem Emailserver zwischengespeichert, jedoch nicht weitergereicht oder zu anderen Zwecken benutzt oder weitergegeben und zweitens von uns gespeichert, um Ihre Fragen zu beantworten. Die in diesem Zusammenhang anfallenden Daten löschen wir, nachdem die Speicherung nicht mehr erforderlich ist, oder schränken die Verarbeitung ein, falls gesetzliche Aufbewahrungspflichten bestehen.</p>
-
 <p>2.3 Falls wir für einzelne Funktionen unseres Angebots auf beauftragte Dienstleister zurückgreifen oder Ihre Daten für werbliche Zwecke nutzen möchten, werden wir Sie untenstehend im Detail über die jeweiligen Vorgänge informieren. Dabei nennen wir auch die festgelegten Kriterien der Speicherdauer.</p>
-
-<h4>3. Ihre Rechte</h4>
-
+<h2>3. Ihre Rechte</h2>
 <p>3.1 Sie haben gegenüber uns folgende Rechte hinsichtlich der Sie betreffenden personenbezogenen Daten:</p>
-
 <ul>
 	<li>Recht auf Auskunft,</li>
 	<li>Recht auf Berichtigung oder Löschung,</li>
@@ -49,15 +32,10 @@ if (!isset($TPL)) {
 	<li>Recht auf Widerspruch gegen die Verarbeitung,</li>
 	<li>Recht auf Datenübertragbarkeit.</li>
 </ul>
-
 <p>3.2 Sie haben zudem das Recht, sich bei einer Datenschutz-Aufsichtsbehörde über die Verarbeitung Ihrer personenbezogenen Daten durch uns zu beschweren. Die Datenschutzaufsichtsbehörde ist der Landesbeauftragte für den Datenschutz und die Informationsfreiheit (LfDI), Königstrasse 10a, 70173 Stuttgart.</p>
-
 <p>3.4 Werden personenbezogene Daten an ein Drittland oder an eine internationale Organisation übermittelt, so haben Sie das Recht, über die geeigneten Garantien gemäß Art. 46 EU-DSGVO im Zusammenhang mit der Übermittlung unterrichtet zu werden.</p>
-
-<h4>4. Erhebung und Zweck personenbezogener Daten bei Besuch unserer Website</h4>
-
+<h2>4. Erhebung und Zweck personenbezogener Daten bei Besuch unserer Website</h2>
 <p>4.1 Bei der bloß informatorischen Nutzung der Website, also wenn Sie sich nicht registrieren oder uns anderweitig Informationen übermitteln, erheben wir nur die personenbezogenen Daten, die Ihr Browser an unseren Server übermittelt. Wenn Sie unsere Website betrachten möchten, erheben wir die folgenden Daten, die für uns technisch erforderlich sind, um Ihnen unsere Website anzuzeigen und die Stabilität und Sicherheit zu gewährleisten (Rechtsgrundlage ist Art. 6 Abs. 1 S. 1 lit. f EU-DSGVO):</p>
-
 <ul>
 	<li>IP-Adresse</li>
 	<li>Datum und Uhrzeit der Anfrage</li>
@@ -70,40 +48,21 @@ if (!isset($TPL)) {
 	<li>Betriebssystem und dessen Oberfläche</li>
 	<li>Sprache und Version der Browsersoftware.</li>
 </ul>
-
 <p>4.2 Zusätzlich zu den zuvor genannten Daten werden bei Ihrer Nutzung unserer Website Cookies auf Ihrem Rechner gespeichert. Bei Cookies handelt es sich um kleine Textdateien, die auf Ihrer Festplatte dem von Ihnen verwendeten Browser zugeordnet gespeichert werden und durch welche der Stelle, die den Cookie setzt (hier durch uns), bestimmte Informationen zufließen. Cookies können keine Programme ausführen oder Viren auf Ihren Computer übertragen. Sie dienen dazu, das Internetangebot insgesamt nutzerfreundlicher und effektiver zu machen.</p>
-
 <p>4.3 Einsatz von Cookies</p>
-
 <p>a) Diese Website nutzt folgende Arten von Cookies, deren Umfang und Funktionsweise im Folgenden erläutert werden:</p>
-
 <p>i. Transiente Cookies (dazu b)</p>
-
 <p>ii. Persistente Cookies (dazu c).</p>
-
 <p>b) Transiente Cookies werden automatisiert gelöscht, wenn Sie den Browser schließen. Dazu zählen insbesondere die Session-Cookies. Diese speichern eine sogenannte Session-ID, mit welcher sich verschiedene Anfragen Ihres Browsers der gemeinsamen Sitzung zuordnen lassen. Dadurch kann Ihr Rechner wiedererkannt werden, wenn Sie auf unsere Website zurückkehren. Die Session-Cookies werden gelöscht, wenn Sie sich ausloggen oder den Browser schließen.</p>
-
 <p>c) Persistente Cookies werden automatisiert nach einer vorgegebenen Dauer gelöscht, die sich je nach Cookie unterscheiden kann. Sie können die Cookies in den Sicherheitseinstellungen Ihres Browsers jederzeit löschen.</p>
-
 <p>d) Sie können Ihre Browser-Einstellung entsprechend Ihren Wünschen konfigurieren und z. B. die Annahme von Third-Party-Cookies oder allen Cookies ablehnen. Wir weisen Sie darauf hin, dass Sie eventuell nicht alle Funktionen dieser Website nutzen können.</p>
-
 <p>e) Wir setzen Cookies ein, um Sie für Folgebesuche identifizieren zu können, falls Sie über einen Account bei uns verfügen. Andernfalls müssten Sie sich für jeden Besuch erneut einloggen.</p>
-
-<h4>Urheberrecht</h4>
-
+<h2>Urheberrecht</h2>
 <p>Texte, Bilder, Grafiken und Fotos sowie die Gestaltung dieser Internetseiten unterliegen dem Urheberrecht. Sie dürfen von Ihnen nur zum privaten und sonstigen eigenen Gebrauch im Rahmen des § 53 Urheberrechtsgesetz (UrhG) ohne jede Änderung vervielfältigt werden. Eine Verbreitung von Kopien (analog oder digital) oder Auszügen ist nur mit schriftlicher Genehmigung zulässig. Der Nachdruck und die Auswertung von Pressemitteilungen und Reden sind mit Quellenangabe gestattet. Texte, Bilder, Grafiken und sonstige Materialien können ganz oder teilweise dem Urheberrecht Dritter unterliegen. Die Inhalte Dritter sind als solche kenntlich gemacht und dürfen ohne Einwilligung des Rechteinhabers nicht vervielfältigt, öffentlich zugänglich gemacht oder öffentlich wiedergegeben werden.</p>
-
-<h4>Haftung für Inhalte</h4>
-
+<h2>Haftung für Inhalte</h2>
 <p>Die Informationen, die Sie auf diesem Internetauftritt vorfinden, wurden nach bestem Wissen und Gewissen sorgfältig zusammengestellt und geprüft. Für die Richtigkeit, Vollständigkeit, Aktualität oder Qualität und jederzeitige Verfügbarkeit der bereit gestellten Informationen wird jedoch keine Gewähr übernommen.</p>
-
 <p>Diensteanbieter sind gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Diensteanbieter sind jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
-
 <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.</p>
-
 <p>Dieser Internetauftritt enthält auch Links oder Verweise auf Internetauftritte Dritter. Diese Links zu den Internetauftritten Dritter stellen keine Zustimmung zu deren Inhalten durch den Herausgeber dar.</p>
-
 <p>Für die Inhalte der externen Links sind die jeweiligen Anbieter oder Betreiber (Urheber) der Seiten verantwortlich. Mit den Links zu anderen Internetauftritten wird den Nutzern lediglich der Zugang zur Nutzung der Inhalte vermittelt.</p>
-
 <p>Die externen Links wurden zum Zeitpunkt der Linksetzung nach bestem Wissen und Gewissen auf eventuelle Rechtsverstöße überprüft. Eine ständige inhaltliche Überprüfung der externen Links ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht möglich. Bei Verlinkungen auf die Webseiten Dritter, die außerhalb des Verantwortungsbereichs des Herausgebers liegen, würde eine Haftungsverpflichtung nur bestehen, wenn der Herausgeber von den rechtswidrigen Inhalten Kenntnis erlangen und es technisch möglich und zumutbar wäre, die Nutzung dieser Inhalte zu verhindern. Sollten Sie der Ansicht sein, dass die verlinkten externen Seiten gegen geltendes Recht verstoßen oder sonst unangemessene Inhalte haben, so teilen Sie uns dies bitte mit.</p>
-</div>
